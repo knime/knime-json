908e6eef676a6311275267964a8f51babf0f97a5
Tobias Urhaug <tobias.urhaug@knime.com>
2018-08-02 16:22:03 +0200

    AP-9907: Added dialog implementation
    AP-9907 (Container Input (Credentials))

--- org.knime.json/src/org/knime/json/node/container/input/credentials/ContainerCredentialsInputNodeDialog.java
+++ org.knime.json/src/org/knime/json/node/container/input/credentials/ContainerCredentialsInputNodeDialog.java
@@ -48,24 +48,95 @@
  */
 package org.knime.json.node.container.input.credentials;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import org.knime.core.data.DataTableSpec;
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.NotConfigurableException;
+import org.knime.core.node.dialog.DialogNode;
 
 /**
+ * Dialog for the Container Input (Credentials) node.
  *
  * @author Tobias Urhaug, KNIME GmbH, Berlin, Germany
  * @since 3.6
  */
 public class ContainerCredentialsInputNodeDialog extends NodeDialogPane {
 
+    private final JFormattedTextField m_parameterNameField;
+    private final JTextArea m_descriptionArea;
+
+    /**
+     * New pane for configuring the Container Input (Table) node.
+     */
+    ContainerCredentialsInputNodeDialog() {
+        m_parameterNameField = new JFormattedTextField();
+        m_parameterNameField.setInputVerifier(DialogNode.PARAMETER_NAME_VERIFIER);
+
+        m_descriptionArea = new JTextArea(1, 20);
+        m_descriptionArea.setLineWrap(true);
+        m_descriptionArea.setPreferredSize(new Dimension(100, 50));
+        m_descriptionArea.setMinimumSize(new Dimension(100, 30));
+
+        addTab("Container Input (Credentials)", createLayout(), false);
+    }
+
+    private JPanel createLayout() {
+        JPanel p = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        p.add(new JLabel("Parameter Name: "), gbc);
+        gbc.gridx += 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1.0;
+        p.add(m_parameterNameField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.weightx = 0;
+        p.add(new JLabel("Description: "), gbc);
+        JScrollPane sp = new JScrollPane(m_descriptionArea);
+        sp.setPreferredSize(m_descriptionArea.getPreferredSize());
+        sp.setMinimumSize(m_descriptionArea.getMinimumSize());
+        gbc.weightx = 1;
+        gbc.gridx++;
+        p.add(sp, gbc);
+
+        return p;
+    }
+
     /**
      * {@inheritDoc}
      */
     @Override
     protected void saveSettingsTo(final NodeSettingsWO settings) throws InvalidSettingsException {
-        // TODO Auto-generated method stub
-
+        ContainerCredentialsInputNodeConfiguration config = new ContainerCredentialsInputNodeConfiguration();
+        config.setParameterName(m_parameterNameField.getText());
+        config.setDescription(m_descriptionArea.getText());
+        config.save(settings);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadSettingsFrom(final NodeSettingsRO settings, final DataTableSpec[] specs) throws NotConfigurableException {
+        ContainerCredentialsInputNodeConfiguration config = new ContainerCredentialsInputNodeConfiguration().loadInDialog(settings);
+        m_parameterNameField.setText(config.getParameterName());
+        m_descriptionArea.setText(config.getDescription());
+    }
 }
